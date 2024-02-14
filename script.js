c5b4c06a88bf1d43ffb7b84046aded4600120588
Winnie3315 <winniiemae777@gmail.com>
2024-02-14 16:07:38 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+//1
+let age = prompt('Сколько вам лет?')
+if(age >= 18 ){
+    alert('welcome')
+}
+else{
+    alert('newelcome')
+}
+//2
+// let name = prompt("Введите ваше имя:");
+// if(name.toLowerCase().startsWith('a')) {
+//     alert("добро пожаловатб");
+// }
+//  else {
+//     alert("гуляй Васек");
+// }
+//3
+// let user = prompt("Введите ваше имя")
+// if(user === 'Alex'){
+//     alert('Alex sila')
+// }
+// else{
+//     alert('самозванец')
+// }
+//4
+// let num = prompt("напишите чтсло")
+//  if (num % 2 === 0) {
+// alert("четное")
+//  }
+//  else {
+//    alert("нечетное")
+//  }
